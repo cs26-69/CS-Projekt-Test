2d20366429055f5012cd14a1aeda660dd1ef58c8
cs26-69 <benjamin.duscio@student.unisg.ch>
2026-03-22 14:10:24 +0100

    Update cs_test.py

--- cs_test.py
+++ cs_test.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title("Hello World!")
 
 st.title("This document is going to be used for our group project.")
+
+st.slider("Input our group grade on a scale from 1 to 6", 1, 6)
