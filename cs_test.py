f34fc2f475dcc4c988d63db6781342d87bc22e0c
cs26-69 <benjamin.duscio@student.unisg.ch>
2026-03-22 14:11:48 +0100

    Update cs_test.py

--- cs_test.py
+++ cs_test.py
@@ -7,4 +7,4 @@ st.title("This document is going to be used for our group project.")
 group_grade = st.slider("Input our group grade on a scale from 1 to 6", 1, 6)
 
 if group_grade == 6:
-  st.write("Good choice")
+  st.write("Good choice :)")
